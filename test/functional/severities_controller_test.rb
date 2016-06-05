6026ea3512d8d7df34465bb4f822774dfaecdc8d
Jose Antonio Parra <josparbar@gmail.com>
2016-06-05 20:13:05 +0200

    Test for severity functionality

--- test/functional/severities_controller_test.rb
+++ test/functional/severities_controller_test.rb
@@ -2,46 +2,45 @@ require 'test_helper'
 
 class SeveritiesControllerTest < ActionController::TestCase
   setup do
-    @severity = severities(:one)
+    @high = severities(:high)
+    @very_low = severities(:very_low)
+    @admin = users(:admin)
+    sign_in @admin
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:severities)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create severity" do
+  test 'should create severity' do
     assert_difference('Severity.count') do
-      post :create, :severity => @severity.attributes
+      post :create, severity: { name: 'new', sig_id: 5, bg_color: '#dddddd',
+                                text_color: '#ffffff' }
     end
 
-    assert_redirected_to severity_path(assigns(:severity))
+    assert_redirected_to severities_path
   end
 
-  test "should show severity" do
-    get :show, :id => @severity.to_param
+  test 'should get edit' do
+    get :edit, id: @high.id
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, :id => @severity.to_param
-    assert_response :success
+  test 'should update severity' do
+    put :update, id: @high.to_param, severity: @high.attributes
+    assert_redirected_to severities_path
   end
 
-  test "should update severity" do
-    put :update, :id => @severity.to_param, :severity => @severity.attributes
-    assert_redirected_to severity_path(assigns(:severity))
-  end
-
-  test "should destroy severity" do
+  test 'should destroy severity' do
     assert_difference('Severity.count', -1) do
-      delete :destroy, :id => @severity.to_param
+      delete :destroy, id: @very_low.to_param
     end
 
     assert_redirected_to severities_path
