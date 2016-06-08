5f2d6b6cf1bf9aa735cdbfc48bf58f47c2559050
Jose Antonio Parra <josparbar@gmail.com>
2016-06-08 20:19:11 +0200

    Add events and signatures fixtures. Tests for Page controller

--- test/functional/page_controller_test.rb
+++ test/functional/page_controller_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class PageControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @admin = users(:admin)
+    sign_in @admin
+  end
+
+  test 'access dashboard' do
+    get :dashboard
+    assert_response :success
+  end
+
+  test 'access search' do
+    get :search
+    assert_response :success
+  end
+
+  test 'get search_json' do
+    get :search_json
+    assert_response :success
+  end
+
+  test 'get cache status' do
+    get :cache_status
+    assert_response :success
   end
 end
