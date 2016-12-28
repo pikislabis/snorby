438c162ca3a5a8a6038f8f2f21a7c80a1a4ab6ac
Jose Antonio Parra <josparbar@gmail.com>
2016-12-29 00:43:58 +0100

    Include sensor's unit tests

--- test/unit/sensor_test.rb
+++ test/unit/sensor_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class SensorTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'A registered sensor' do
+    setup do
+      @sensor = sensors(:one)
+    end
+
+    should 'gets its caches' do
+      assert_equal @sensor.cache.count, Cache.where(sid: @sensor.id).count
+    end
+
+    should 'return its name' do
+      assert_equal @sensor.sensor_name, @sensor.name
+    end
+
+    should 'gets its daily caches' do
+      assert_equal @sensor.daily_cache.count,
+                   DailyCache.where(sid: @sensor.id).count
+    end
+  end
+
+  context 'A new sensor' do
+    setup do
+      @sensor = sensors(:two)
+    end
+
+    should 'returns hostname instead of name' do
+      assert_equal @sensor.sensor_name, @sensor.hostname
+    end
   end
 end
